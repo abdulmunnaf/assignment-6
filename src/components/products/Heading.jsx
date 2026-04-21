1e9d5b1b663a53c6d394836e859e7a2ffff8f117
abdulmunnaf <amunnaf799@gmail.com>
2026-04-21 18:31:08 +0600

    add Heading component for displaying premium digital tools information

---
+++ src/components/products/Heading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Heading = () => {
+    return (
+        <div className='mt-10 md:mt-20 w-full md:w-10/12 mx-auto text-center my-4'>
+
+            <h1 className='font-bold text-3xl md:text-5xl mb-3 '>Premium Digital Tools</h1>
+            <p className='text-gray-500 text-xs px-2 md:px-0 line-clamp-3 md:text-sm mb-3'> Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity.
+            </p>
+        </div>
+    );
+};
+
+export default Heading;
